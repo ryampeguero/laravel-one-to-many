7fb65c7c0711f51fc0e4a2b364cb3e93092b77d3
ryampeguero <ryampeguero@gmail.com>
2024-06-24 20:52:25 +0200

    feat: added preview image in edit with javascript

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,3 +2,29 @@ import "./bootstrap";
 import "~resources/scss/app.scss";
 import.meta.glob(["../img/**"]);
 import * as bootstrap from "bootstrap";
+
+
+const preview = document.getElementById('preview');
+const imgPath = document.getElementById('image');
+const btnPreview = document.getElementById('show-preview');
+
+btnPreview.addEventListener('click', (event)=>{
+    event.preventDefault();
+    preview.innerHTML = "";
+    const imgPreview = new Image();
+
+    let file = imgPath.files; //Leggo i file contenuti nell'input
+    console.log(file);
+    
+    const reader = new FileReader(); //Istanza della Classe FileReader
+    
+    reader.onload = async (event) => { //Carico asincro
+        imgPreview.setAttribute('src', event.target.result)
+    }
+    
+    reader.readAsDataURL(file[0]);
+    
+    preview.append(imgPreview);
+});
+
+console.log(imgPreview);
